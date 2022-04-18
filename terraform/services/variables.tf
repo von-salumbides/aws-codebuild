5c9beacb83ca1296dab79036c8b6ed2e4ace5e74
von-salumbides <vonsalumbides.devops@gmail.com>
2022-04-18 11:48:40 +0800

    aws-codebuild project

---
+++ terraform/services/variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+  default = "devops"
+}
+
+variable "owner" {
+  default = "devops"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "cost_center" {
+  default = "common"
+}
+
